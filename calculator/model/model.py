38cc786f4e2602108f8238cb6456c9b3dad063d1
RaulCasado <raulcasado969@gmail.com>
2024-05-22 11:33:26 +0200

    refactor: change model to use numexpr instead of eval

--- calculator/model/model.py
+++ calculator/model/model.py
@@ -1,24 +1,28 @@
 from exceptions.parenthesis_exception import ParenthesisError
+import numexpr as ne
+
 class Calculator:
     def __init__(self):
         self.previous_value = 0
 
-    def make_calculation(self,operation):
+    def make_calculation(self, operation):
         if operation == "":
             return str(self.previous_value)
         
         if not self.is_balanced(operation):
             raise ParenthesisError
-    
-        if self.previous_value == 0:
-            resolved_operation = eval(operation)
+        
+        try:
+            if self.previous_value == 0:
+                resolved_operation = ne.evaluate(operation)
+            else:
+                current_operation = f"{self.previous_value}{operation}"
+                resolved_operation = ne.evaluate(current_operation)
+            
             self.previous_value = resolved_operation
             return str(resolved_operation)
-        
-        current_operation = str(self.previous_value) + operation
-        resolved_operation = eval(current_operation)
-        self.previous_value = resolved_operation
-        return str(resolved_operation)
+        except Exception as e:
+            raise ValueError("Revise la cuenta por favor")
     
     def is_balanced(self,input_string):
         parentheses_map = {'(': ')'}
